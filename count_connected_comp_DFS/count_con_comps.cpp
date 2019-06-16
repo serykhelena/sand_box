65bee5e023a741eab65378a05e15d52533d96e3c
Lena <seryhelena@gmail.com>
2019-06-16 20:43:11 +0300

    remove comments

--- count_connected_comp_DFS/count_con_comps.cpp
+++ count_connected_comp_DFS/count_con_comps.cpp
@@ -13,14 +13,6 @@ public:
 	RandomGraph(int16_t n)
 	{
 		v_number = n; 
-		cout << "INITIAL GRAPH" << endl; 
-		
-		// graph.push_back({0, 1, 1, 0});
-		// graph.push_back({1, 0, 0, 1});
-		// graph.push_back({1, 0, 0, 1});
-		// graph.push_back({0, 1, 1, 0});
-
-
 		for (int16_t x = 0; x < n; x++)
 		{
 			graph.push_back({ });
@@ -28,12 +20,9 @@ public:
 			{
 				graph[x].push_back(rand() % 2); 
 				if (x == y) graph[x][y] = 0;
-				
-				cout << graph[x][y] << " ";	// DON'T FORGET TO REMOVE!! 
 			}
-			cout << "\n";					// DON'T FORGET TO REMOVE!! 
 		}
-		cout << "MODIFIED GRAPH" << endl; 
+		// to make from random array -> adjacency matrix
 		for( int16_t x = 0; x < n; x++ )
 		{
 			for( int16_t y = 0; y < n; y++ )
@@ -45,28 +34,13 @@ public:
 						graph[y][x] = graph[x][y] = 1; 
 					}
 				}
-
+				// fill set of vertexes 
 				if (graph[x][y] != 0)
 				{
 					edges[x + 1].push_back(y + 1); 
 				}
-				cout << graph[x][y] << " ";
 			}
-			cout << "\n";					// DON'T FORGET TO REMOVE!! 
 		}
-
-		cout << "EDGES" << endl; 
-		for( auto item : edges )
-		{
-			cout << item.first << " : "; 
-			for( int16_t i = 0; i < item.second.size(); i++)
-			{
-				cout << item.second[i] << " ";
-			}
-			cout << "\n";
-		}
-
-
 	}
 
 	int16_t v_number; 
@@ -82,12 +56,9 @@ void dfs(int16_t& cur_v, RandomGraph& g)
 	marked_v.insert(cur_v);
 	for (size_t i = 0; i < g.edges[cur_v].size(); i++) 
 	{
-		cout << "dfs i: " << i << " | cur_v: " << cur_v
-			 << " next v: " << cur_v + 1 << endl; 
 		int16_t next = g.edges[cur_v][i];	
-		if (!marked_v.count(next) /*&& g.graph[cur_v - 1][i] == 1*/)	// if we don't check this vertix 
+		if ( !marked_v.count(next) ) // if we don't check this vertex 
 		{
-			cout << "dfs recursive - ok " << endl;
 			dfs(next, g);
 		}
 	}
@@ -96,13 +67,11 @@ void dfs(int16_t& cur_v, RandomGraph& g)
 int16_t connected_comps_count( RandomGraph& g)
 {
 	int16_t comp_count = 0; 
-	for (int16_t i = 1; i <= g.v_number; i++)	// check all vertxes 
+	for (int16_t i = 1; i <= g.v_number; i++)	// check all vertexes 
 	{
-		cout << "v: " << i << " | count: " << comp_count << endl;
 		if (!marked_v.count(i))
 		{
 			dfs(i, g);
-			cout << "comp ++ " << endl;
 			if( i != g.v_number) comp_count += 1; 
 		}
 	}
@@ -111,13 +80,11 @@ int16_t connected_comps_count( RandomGraph& g)
 
 int main()
 {
-
-	// cin << n; 
 	const int16_t n = 4;
 
 	RandomGraph g(n); 
 
-	cout << connected_comps_count(g) << endl; 
+	cout <<"The number of connected components: " << connected_comps_count(g) << endl; 
 	
 	return 0;
 }
