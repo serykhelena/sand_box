7105e99130e2ae87022a182868a91c6677ec631f
Unicorn User <nonexistingemail@gmail.com>
2019-06-24 17:20:27 +0300

    quick sort is working

--- py_algorithms/quick_sort.py
+++ py_algorithms/quick_sort.py
@@ -6,53 +6,51 @@ import time
 	https://tproger.ru/translations/sorting-algorithms-in-python/
 '''
 
-def partition(data, low, high):
+def quick_sort_eat_memory(data):
 
-	pivot = data[(low + high) // 2]
-	i = low
-	j = high
+	if len(data) <= 1:
+		return data
+	else:
 
-	while True: 
+		i = 0
+		j = len(data)-1
+		pivot = data[(i + j) // 2]
+		left = []
+		mid = []
+		right = []
 
-		print(f"{i} :: {j}")
-		while data[i] < pivot:
-			i += 1 
+		for d in data:
+			if d < pivot:
+				left.append(d)
+			elif d > pivot:
+				right.append(d)
+			else:
+				mid.append(d)
 
-		while data[j] > pivot:
-			j -= 1
-
-		if i <= j:
-			data[i], data[j] = data[j], data[i] 
-			i += 1
-			j -= 1
-		if i <= j:
-			break 
-
-	return j 
-
+		return quick_sort(left) + mid + quick_sort(right)
 
 	
-	# while True:
-	# 	i += 1
-	# 	while data[i] < pivot:
-	# 		i += 1 
+def quick_sort_economy(data, low, high):
+	if low >= high:
+		return 
+	else:
+		pivot = data[(low+high) // 2]
+		i = low
+		j = high
 
-	# 	j -= 1
-	# 	while data[j] > pivot: 
-	# 		j -= 1
+		while i <= j:
+			while data[i] < pivot:
+				i += 1
 
-	# 	if i >= j:
-	# 		return j 
+			while data[j] > pivot:
+				j -= 1
 
-	# 	data[i], data[j] = data[j], data[i] 
-
-def quick_sort(data):
-	def _quick_sort(items, low, high):
-		split_indx = partition(items, low, high)
-		_quick_sort(items, low, split_indx)
-		_quick_sort(items, split_indx+1, high)
-
-	return _quick_sort(data, 0, len(data)-1)
+			if i <= j: 
+				data[i], data[j] = data[j], data[i]
+				i += 1
+				j -= 1
+				quick_sort_economy(data, low, j)
+				quick_sort_economy(data, i, high)
 
 
 def runner():
@@ -61,8 +59,8 @@ def runner():
 	rand_list = random.sample(range(10), 5)
 	print(f"The initial data: {rand_list}")
 
-	result = quick_sort(rand_list)
-	print(f"The result data: {result}")
+	result = quick_sort_economy(rand_list, 0, len(rand_list)-1)
+	print(f"The result data: {rand_list}")
 
 	end = time.time()
 	print(f"Time of execution: {round(end - start, 5)} s")
