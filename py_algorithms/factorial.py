b1352eb6ddb71c6ba4df3796e2fee4654f3398c5
Unicorn User <nonexistingemail@gmail.com>
2019-10-04 09:33:45 +0300

    add factorial

---
+++ py_algorithms/factorial.py
@@ -0,0 +1,12 @@
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+if __name__ == "__main__":
+    for i in range(25):
+        print(factorial(i))
