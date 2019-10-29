ab9e635556e0a0c843efb85c8d92c6ac82d0b78f
Unicorn User <nonexistingemail@gmail.com>
2019-10-29 19:03:33 +0300

    simple parsing derivative work

--- cpp_algorithms/derivative_parser/derivative/main.cpp
+++ cpp_algorithms/derivative_parser/derivative/main.cpp
@@ -2,11 +2,13 @@
 #include <map>
 #include <string>
 
+#include <bits/stdc++.h>
+
 #include <cassert>
 
 using namespace std;
 
-string derivate(string input)
+string derivate(string polynomial)
 {
     map<int, signed int> data;
 
@@ -15,129 +17,102 @@ string derivate(string input)
     string k;
     string result = "";
     bool been = false;
+    int64_t pos = 0;
 
 
-    while( input.length() > 0 )
+    while( polynomial.length() > 0 )
     {
-        int16_t pos = input.find('+') == string::npos ? input.find('-') : input.find('+');
-
-        if( input.find('+') < input.find('-') )
-            pos = input.find('+');
-        else
+        /* split statement by term */
+        if( polynomial.find('+') < polynomial.find('-') && polynomial.find('+') != string::npos )
+            pos = polynomial.find('+');
+        else if( polynomial.find('-') != string::npos ) // if thre is minus in statement
         {
-            pos = input.find('-');
-//        cout << "POS 28 " << pos << endl;
-
-            if( input.find('-') != string::npos )
+            pos = polynomial.find('-');
+            if( pos == 0 ) // if statement starts with minus
             {
-    //            cout << "MINUS HERE" << endl;
-                pos = input.find('-');
-                if( pos == 0 )
-                {
-                    string tmp = input.substr(1, input.length());
-                    if( tmp.find('+') != string::npos )
-                        pos = tmp.find('+') + 1;
-                    else
-                        pos = tmp.find('-') + 1;
-                }
+                string tmp = polynomial.substr(1, polynomial.length());
+                if( tmp.find('+') < tmp.find('-') && tmp.find('+') != string::npos )
+                    pos = tmp.find('+') + 1;
+                else
+                    pos = tmp.find('-') + 1;
+
+                if( pos == 0 ) pos = polynomial.length();
             }
         }
-//        cout << "POS " << pos << endl;
-        if( input.find('+') !=  string::npos )
-        {
-            part = input.substr(0, pos);
-        }
-        else if( input.find('-') != string::npos && pos != 0 )
-            part = input.substr(0, pos);
         else
-        {
-            part = input.substr(0, input.length());
-        }
+            pos = polynomial.length();
 
-//        cout << "Part is " << part << endl;
-
+        part = polynomial.substr(0, pos);
+        /* new part */
         power = "";
         k = "";
 
         if( part.find('^') != string::npos )
         {
-            uint16_t mult_pos = part.find('^');
-            for( uint16_t i = mult_pos + 1; i < part.length(); i++ )
+            uint64_t mult_pos = part.find('^');
+            for( uint64_t i = mult_pos + 1; i < part.length(); i++ )
             {
-                if( i != '*')
-                    power += part[i];
-                else
-                    break;
+                if( i != '*') power += part[i];
+                else break;
             }
-//            cout << "Found ^ " << power << ": " << data[stoi(power)] << endl;
             data[stoi(power)] += 0; // as default
-//            cout << "After Found " << power << ": " << data[stoi(power)] << endl;
         }
         if( part.find('*') != string::npos )
         {
-            uint16_t x_pos = part.find('*');
-            for( uint16_t i = 0; i < x_pos; i++ )
+            uint64_t x_pos = 0;
+            string tmp = part;
+            string val = "";
+            k = "1";
+            for( auto j = 0; j < count(part.begin(), part.end(), '*'); j++ )
             {
-                k += part[i];
+                x_pos = tmp.find('*');
+                for( uint64_t i = 0; i < x_pos; i++ )
+                {
+                    val += tmp[i];
+                }
+                tmp = part.substr(x_pos + 1, part.length());
+                k = to_string(stoi(k) * stoi(val));
+                val = "";
             }
-            if( /*data.size() == 0 ||*/ power == "" ) power = "1";
-//            if( part[0] == 0 ) data[stoi(power)] += -stoi(k); // as default
-            /*else */data[stoi(power)] += stoi(k); // as default
+
+            if( power == "" ) power = "1";
+            data[stoi(power)] += stoi(k); // as default
         }
         if( part[0] == '-' && part.find('x') == 1 && part.find('*') == string::npos)
         {
-
+            if( power == "" ) power = "1";
             data[stoi(power)] += -1;
-//            cout << "MINUSSSS " << power << ": " << data[stoi(power)] << endl;
         }
-
-        if( part.find('x') != string::npos && part.find('*') == string::npos
-            && part.find('^') == string::npos)
+        else if( part.find('x') != string::npos && part.find('*') == string::npos
+            && part.find('^') == string::npos )
         {
-
             data[1] += 1;
         }
         else if( k == "" && power != "" && part[0] != '-')
-        {
-//            cout << "Else if" << endl;
             data[stoi(power)] += 1;
-        }
 
-//        for( auto iter = data.rbegin(); iter != data.rend(); iter++ )
-//        {
-//            cout << iter -> first << ": " << iter -> second << endl;
-//        }
-
-
-
-        pos = pos < 0 ? input.length() - 1 : pos;
-//        cout << "POS POS POS " << pos << " " << input << " " << input[pos] << endl;
-        if( input[pos] == '-' )
+        if( pos == static_cast<int64_t>(polynomial.length()) )
+            polynomial = "";
+        else if( polynomial[pos] == '-' )
         {
             if( been )
             {
-                string tmp = input.substr(1, input.length());
-                pos = tmp.find('-');
-//                cout << "BEEN " << pos << endl;
-                input = "";
-                break;
+                string tmp = polynomial.substr(1, polynomial.length());
+                pos = tmp.find('-') + 1;
             }
-            been = true;
-//            cout << "first been " << endl;
-            input = input.substr(pos, input.length());
+            else been = true;
+
+            polynomial = polynomial.substr(pos, polynomial.length());
         }
         else
         {
-//            cout << "else end " << pos << endl;
-            input = input.substr(pos + 1, input.length());
+            polynomial = polynomial.substr(pos + 1, polynomial.length());
             been = false;
         }
-
-//        cout << "NEW INPUT is " << input << endl;
     }
+
     for( auto iter = data.rbegin(); iter != data.rend(); iter++ )
     {
-//        cout << iter -> first << ": " << iter -> second << endl;
         if( iter -> first == 2 && iter -> second != 0)
         {
             result += to_string(iter -> first * iter -> second) + "*x";
@@ -147,7 +122,7 @@ string derivate(string input)
             if( result.size() == 0 )
                 result += to_string(iter -> first * iter -> second) + "*x";
             result += "^" + to_string(iter -> first - 1);
-            if( iter -> second > 0 )
+            if( iter -> second > 0 && data.size() > 1 )
                 result += "+";
         }
         else if( iter -> second == 0 )
@@ -156,6 +131,8 @@ string derivate(string input)
             {
                 result = "0";
             }
+            else if( data.size() == 1 )
+                result = to_string(iter -> first * iter -> second);
             else
                 continue;
         }
@@ -174,11 +151,9 @@ string derivate(string input)
                 else
                     result += + "+" + to_string(iter -> first * iter -> second );
             }
-
         }
     }
     if( data.size() == 0 ) result = "0";
-//    cout << "RESULT!!!!!!!! " << result << endl;
     return result;
 }
 
@@ -228,14 +203,29 @@ int main()
     assert(derivate("1+x^2+1+0") == "2*x");
     cout << "x^2+1\t\t\tpassed\tOK" << endl;
     assert(derivate("100*x-20*x+10*x-50*x") == "40");
-    cout << "100*x-20*x+10*x-50*x\t\tpassed\tOK" << endl;
-
-//    cout << derivate("x^3+2*x^2-3*x+5") << endl;
-//    cout << derivate("10") << endl;
-
-
-
-
+    cout << "100*x-20*x+10*x-50*x\tpassed\tOK" << endl;
+    assert(derivate("-x^2+x^2+x") == "1");
+    cout << "-x^2+x^2+x\t\tpassed\tOK" << endl;
+    assert(derivate("-x^2-x^2+x") == "-4*x+1");
+    cout << "-x^2-x^2+x\t\tpassed\tOK" << endl;
+    assert(derivate("-x^2-x^2-x") == "-4*x-1");
+    cout << "-x^2-x^2-x\t\tpassed\tOK" << endl;
+    assert(derivate("x-x-x-x-x-x-x-x-x-x") == "-8");
+    cout << "x-x-x-x-x-x-x-x-x-x\tpassed\tOK" << endl;
+    assert(derivate("-x-x-x-x-x-x-x-x-x-x") == "-10");
+    cout << "-x-x-x-x-x-x-x-x-x-x\tpassed\tOK" << endl;
+    assert(derivate("-7*0*x") == "0");
+    cout << "-7*0*x\t\t\tpassed\tOK" << endl;
+    assert(derivate("-1*x^2-2*x^3-3*x^2") == "-6*x^2-8*x");
+    cout << "-1*x^2-2*x^3-3*x^2\tpassed\tOK" << endl;
+    assert(derivate("-1*x^1-2*x^3-3*x^2") == "-6*x^2-6*x-1");
+    cout << "-1*x^1-2*x^3-3*x^2\tpassed\tOK" << endl;
+    assert(derivate("-x-x-1256895") == "-2");
+    cout << "-x-x-1256895\t\tpassed\tOK" << endl;
+    assert(derivate("x-1") == "1");
+    cout << "x-1\t\t\tpassed\tOK" << endl;
+    assert(derivate("x^600000-1") == "600000*x^599999");
+    cout << "x^600000-1\t\tpassed\tOK" << endl;
 
     return 0;
 }
