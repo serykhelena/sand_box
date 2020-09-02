38385a6e5f2f978f43e0e8872107f1dc0a68ebb3
Lena <seryhelena@gmail.com>
2020-09-02 16:22:47 +0300

    python fibo.py fixed

--- py_algorithms/fibo.py
+++ py_algorithms/fibo.py
@@ -1,33 +1,19 @@
 
+def even_fib(num):
+    '''
+        Get only even numbers from Fibonacci sequence 
 
-def fib_even_only(num):
-	'''
-		Return only even numbers from Fibonacci sequence
-
-		Args: 
-			num 	- number of even values 
-
-		Return
-			[list]	- array with even-only values  
-	'''
-    counter = 1 
-    res = [0]
-    temp = [1, 1]
-    if num <= 0: 
-    	return None 
-    
-    while counter < num: 
-        cur_val = temp[len(temp) - 2] + temp[len(temp) - 1]
-        temp[0], temp[1] = temp[1], cur_val
-        
-        if cur_val % 2 == 0:
-            res.append(cur_val)
-            counter += 1 
-
-    return res 
+        args:       
+            num     - number of even numbers 
+        return:
+            [list]  - list with only even numbers     
+    '''
+    return [fib(i * 3) for i in range(num)]
 
 
 def fib(pos):
+    if pos == 0:
+        return 0
     if pos in (1, 2):
         return 1
     else:
@@ -39,7 +25,5 @@ def print_fib(num):
         print(fib(i), end=' ')
 
 if __name__ == "__main__":
-    print_fib(3)
-    print('\n')
-    print(fib_even_only(0), end=' ')
+    print(even_fib(4))
 
